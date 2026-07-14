6224c34af5b55a3c4eac72b74ac6d457ae199e2d
Ju-Min-Kyung <alsrud7767@naver.com>
2026-07-14 17:59:34 +0900

    design: 글쓰기 화면 UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
 import CategoryBoard from "../components/CategoryBoard.vue";
 import CtaSection from "../components/CtaSection.vue";
 
-
 const scrollToCategories = () => {
   document.querySelector("#category-board")?.scrollIntoView({
     behavior: "smooth",
@@ -10,54 +9,33 @@ const scrollToCategories = () => {
 };
 </script>
 
-
 <template>
   <main>
-
     <section class="hero">
       <div class="hero__shade"></div>
 
       <div class="hero__content container">
-
-        <span class="hero__badge">
-          ⌾ 대전 권역 로컬 커뮤니티
-        </span>
-
+        <span class="hero__badge"> ⌾ 대전 권역 로컬 커뮤니티 </span>
 
         <h1 class="hero__title">
           대전의 진짜 정보,<br />
           여기 townly에 다 있어요
         </h1>
 
-
-        <p class="hero__description">
-          공공데이터에 흩어진 맛집 · 명소 · 축제 정보를 한 곳에서.
-        </p>
-
+        <p class="hero__description">공공데이터에 흩어진 맛집 · 명소 · 축제 정보를 한 곳에서.</p>
 
         <div class="hero__actions">
-
-          <button
-            class="button button--primary"
-            @click="scrollToCategories"
-          >
+          <button class="button button--primary" @click="scrollToCategories">
             게시판 둘러보기
           </button>
 
-
-          <button class="button button--secondary">
-            지도로 보기
-          </button>
-
+          <button class="button button--secondary">지도로 보기</button>
         </div>
-
       </div>
     </section>
-
 
     <CategoryBoard />
 
     <CtaSection />
-
   </main>
 </template>
